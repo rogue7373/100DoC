8a61565f9550712615072796226fef2ba0bb442e
TheRogue <36058852+rogue7373@users.noreply.github.com>
2021-11-21 02:01:57 -0700

    Adding Day 7 BMI Calculator.

--- Day7/main.py
+++ Day7/main.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python
 
+# Don't change the code below. 
+
+height = input("Enter your height in m: ")
+
+weight = input("Enter your weight in kg: ")
+
+# Don't chage the code above 
+
+height_as_float = float(height)
+weight_as_int = int(weight)
+
+# Using the exponent operator ** 
+
+bmi = weight_as_int / height_as_float ** 2
+
+# or using multiplicaiton and PEMDAS
+bmi = weight_as_int / (height_as_float * height_as_float)
+
+bmi_as_int = int(bmi)
+
+print("Your calculated BMI is: " + str(bmi_as_int))
+
+
+
+
