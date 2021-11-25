8660abcbdc9f028830b47b03d303427d2e512e61
TheRogue <36058852+rogue7373@users.noreply.github.com>
2021-11-24 22:59:16 -0700

    Adding Day 9

--- Day9/main.py
+++ Day9/main.py
@@ -24,3 +24,23 @@ if number % 2 == 0:
     (print("Your number is even"))
 else:
     (print("Your number is odd"))
+
+
+# Nested Statements and elif statements 
+
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You are tall enough to ride the rollercoaster!")
+    age = int(input("How old are you? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif age < 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else: 
+    print("Sorry, you are too short to ride the rollercoaster!")
+
+    
