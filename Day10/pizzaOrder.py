bbcc872fb7dd6ee92846b6941be647bb1ee0eee3
TheRogue <36058852+rogue7373@users.noreply.github.com>
2021-11-26 10:31:59 -0700

    Adding Day 10, second assignment Pizza Order

---
+++ Day10/pizzaOrder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Pizza Order PoS System
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, L ")
+add_pepperoni = input("Do you want pepperoni? Y/N ")
+extra_cheese = input("Do you want extra cheese? Y/N ")
+
+
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is ${bill}")
+
+
